3bf6d44b535625f4254a49c58df42c55b09dcba7
Paweł__ <pawelkoc681@gmail.com>
2023-03-12 00:25:01 +0100

    2 bardziej skomplikowana wersja

--- main.py
+++ main.py
@@ -4,10 +4,24 @@ import time
 print('wybierz liczbe od 0 do 10 ')
 time.sleep(5)
 
-while True:
-    guess = random.randrange(11)
+modulo = input('Czy twoja liczba jest parzysta? : ')
+modulo.lower()
+lower_higher = input('Czy twoja liczba jest większa od 5 ?: ')
+lower_higher.lower()
 
-    bol = input('Czy ' + str(guess) + ' to twoja liczba :')
+while True:
+    if modulo == 'tak':
+        if lower_higher == 'tak' :
+            guess = random.choice([6,8,10])
+        elif lower_higher == 'nie':
+            guess = random.choice(0,2,4)
+    elif modulo == 'nie':
+        if lower_higher == 'tak':
+            guess = random.choice([7,9])
+        elif lower_higher == 'nie':
+            guess = random.choice([1,3,5])
+
+    bol = input('Czy ' + str(guess) + ' to twoja liczba : ')
     bol = bol.lower()
     if bol == 'tak':
         print('Wygrana !')
